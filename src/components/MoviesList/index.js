fca394146d4c385f710a70e7270d2a021c676255
diegom0ta <motadiego98@gmail.com>
2020-09-18 10:11:07 -0300

    create new component and add routes

--- src/components/MoviesList/index.js
+++ src/components/MoviesList/index.js
@@ -1,63 +1,85 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { fetchMovies } from '../../api/fetchMovies';
 import '../../css/atlasflix.css';
+import MovieDetails from '../MovieDetails';
 
 export default class MoviesList extends Component {
 	state = {
-		loading: '',
+		favorites: [],
+		loading: false,
 		title: '',
-		movies: []
+		movies: ''
 	};
 
 	handleSearch(e) {
 		const value = e.target.value;
-		fetchMovies(value).then((res) => {
-			res
-				.json()
-				.then((data) => {
-					this.setState({
-						title: value,
-						movies: data
-					});
-				})
-				.catch((err) => console.log('Error', err));
-		});
+		fetchMovies(value)
+			.then((res) => {
+				res
+					.json()
+					.then((data) => {
+						this.setState({
+							title: value,
+							movies: data
+						});
+					})
+					.catch((err) => console.log('Error', err));
+			})
+			.catch((err) => console.log('Error', err));
 	}
 
-	movieInfo = () => {
-		const { movies, title } = this.state;
-
-		return title !== null ? (
-			<>
-				<h2>
-					<i>Title:</i> {movies.Title}
-				</h2>
-				<h4>
-					<i>Year:</i> {movies.Year}
-				</h4>
-			</>
-		) : (
-			<h4>Movie not found!</h4>
-		);
+	addFavorite = (e) => {
+		const favorite = this.state.movies;
+		this.setState({
+			favorites: [...this.state.favorites, favorite]
+		});
+		console.log('fav', this.state.favorites);
+		e.preventDefault();
 	};
 
 	render() {
 		return (
-			<div>
+			<Router>
 				<div>
-					<form>
-						<input
-							className='text-field'
-							id='search'
-							type='text'
-							placeholder='Search by movie title'
-							value={this.state.title}
-							onChange={(e) => this.handleSearch(e)}
-						/>
-					</form>
+					<div className='page-title'>
+						<h1>AtlasFlix</h1>
+					</div>
+					<div>
+						<form>
+							<input
+								className='text-field'
+								id='search'
+								type='text'
+								placeholder='Search by movie title'
+								value={this.state.title}
+								onChange={(e) => this.handleSearch(e)}
+							/>
+							<button
+								onClick={(e) => this.addFavorite(e)}
+								type='button'
+								title='Like!'
+							>
+								Like!
+							</button>
+						</form>
+					</div>
+					<div>
+						<Link to={`/${this.state.movies.Title}`}>
+							<h2>{this.state.movies.Title}</h2>
+						</Link>
+						<h4>{this.state.movies.Year}</h4>
+					</div>
+					<Switch>
+						<Route path='/'>
+							<MoviesList />
+						</Route>
+						<Route path={`/${this.state.movies.Title}`}>
+							<MovieDetails {...this.state.movies} />
+						</Route>
+					</Switch>
 				</div>
-				<div>{this.movieInfo()}</div>
-			</div>
+			</Router>
 		);
 	}
 }
